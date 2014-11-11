18b096aa083df1ee955a5ec1c8c8941de6a89b96
nauman muhammad <nauman.muhammad@nxb.com.pk>
2014-11-11 10:50:57 +0500

    Refactor Workflow Load

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   has_and_belongs_to_many :states
+
+  TASK_TYPE_BEFORE = 'before'
+  TASK_TYPE_AFTER = 'after'
 end
--- config/initializers/load_workflows.rb
+++ config/initializers/load_workflows.rb
@@ -1,36 +1,25 @@
 Workflow.all.each do |workflow|
   if workflow.active
     workflow.transitions.each do |transition|
-      puts "From State #{transition.from_state.name.to_sym}"
-      puts "To State #{transition.to_state.name.to_sym}"
       "#{workflow.workflow_type}StateMachine".constantize.transition from: transition.from_state.name.to_sym, to: transition.to_state.name.to_sym
 
       "#{workflow.workflow_type}StateMachine".constantize.before_transition from: transition.from_state.name.to_sym, to: transition.to_state.name.to_sym do  |entity, state_machine_transition|
-        puts 'In Before Transition'
         state = State.find_by_name state_machine_transition.to_state.to_s
         unless state.tasks.nil?
           state.tasks.each do |task|
-            puts "I am Performing an Action Before entering #{state_machine_transition.to_state.to_s.capitalize} State."
-            entity.send task.name if task.task_type == 'before'
+            entity.send task.name if task.task_type == Task::TASK_TYPE_BEFORE
           end
         end
-        puts 'Before Transition Callback Registering Completed'
       end
 
       "#{workflow.workflow_type}StateMachine".constantize.after_transition from: transition.from_state.name.to_sym, to: transition.to_state.name.to_sym do |entity, state_machine_transition|
-        puts 'In After Transition'
         state = State.find_by_name state_machine_transition.to_state.to_s
         unless state.tasks.nil?
           state.tasks.each do |task|
-            entity.send task.name if task.task_type == 'after'
+            entity.send task.name if task.task_type == Task::TASK_TYPE_AFTER
           end
         end
-        puts 'After Transition Callback Registering Completed'
       end
-
-      puts "All states #{LeadStateMachine.states}"
-      puts "Current State: #{Lead.new.current_state}"
-      puts "Allowed State: #{Lead.new.state_machine.allowed_transitions}"
 
     end
   end
